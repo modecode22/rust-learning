79345c09c3be97f83d014dd7b9589eca6b93b8f1
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-03-31 10:27:26 +0100

    learning rust

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+
+
+//*basic data types*/  
+
+// Boolean 
+// let is_active: bool = true;
+
+// Integer 
+// let age: u8 = 30
+
+
+// Float 
+// let pi: f64 = 3.14
+
+// Double 
+// let e: f64 = 2.718281828459045
+
+// Character 
+// let c: char = 'c';
+
+// String 
+// let name: String = "Moncef"
+
+
+// Arrays 
+// let numbers: [i32; 5] = [1,2,3,4,5];
+
+// Tuple 
+// let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+
+// Struct 
+// struct Person {
+//     name: String,
+//     age: u8
+// }
+
+
+//*variables */
+
+//? by default variables are not mutable (you cant change them) , you can change that by passing (mut) after let and let them mutable
+
+// let two  = 2;
+// let hello = "Hello";
+// let j = 'j';
+// let my_half = 0.5;
+// let my_bool = true;
+// let mut my_name = "Moncef";
+// let your_half = my_half;
+
+
+//*Functions */
+// fn name(a: i32, b: i32) -> i32 {
+//      a + b //function body
+//  }
+
+// let x = (1,1);
+// let y = (3,0);
+// let z = (x,1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn main() {
+ fn name(a: i32, b: i32) -> i32 {
+     a + b //function body
+ }
+
+let x = (1,1);
+let y = (3,0);
+let z = (x,1);
+    println!("fdfsfsdf");
+
+}
+
+
+
+
+
+
+
+
+
+
